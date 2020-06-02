e8c8d6ec77494abf90749c3b52c8c6a5adbe298b
Michael Yu <michael.yu2016@gmail.com>
2020-06-01 19:42:49 -0700

    testing email link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,49 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Preview from "../components/preview"
 import EmailLink from "../components/email-link"
+import { buildEmailPreview } from "../emails/email-builder"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Preview emailId={"police-brutality-la"} />
-    <EmailLink
-      recipients={[
-        "michaelnyu@yahoo.com",
-        "bccemail@email.com",
-        "bccemail2@email.com",
-      ]}
-      subject={"yes"}
-      body={"this is body"}
-    />
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const [emailId, setEmailId] = useState("")
+  const [emailBody, setEmailBody] = useState("")
+  const [emailSubject, setEmailSubject] = useState("")
+  const [emailRecipients, setEmailRecipients] = useState("")
+
+  // FOR TESTING
+  // what inputs & selects would probably do
+  useEffect(() => {
+    setEmailId("police-brutality-la")
+    setEmailRecipients([
+      "michaelnyu@yahoo.com",
+      "bccemail@email.com",
+      "bccemail2@email.com",
+    ])
+
+    const email = buildEmailPreview({
+      emailId,
+      stringInputs: { name: "michael" },
+    })
+    setEmailSubject("this is the subject")
+    setEmailBody(email.body)
+  }, [emailId])
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Preview emailId={emailId} />
+      <EmailLink
+        recipients={emailRecipients}
+        subject={emailSubject}
+        body={emailBody}
+      />
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
