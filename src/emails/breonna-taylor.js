c735757aaff0d2bf5c5fd51092492f3a9fb4e7de
Bryan Wong <bryanw0ng@ucla.edu>
2020-06-03 14:08:24 -0700

    add receivers to templates

---
+++ src/emails/breonna-taylor.js
@@ -0,0 +1,39 @@
+function buildEmail({ name, state }) {
+    return {
+      title: "Justice for Breonna Taylor Template",
+      subject: "Justice for the murder of Breonna Taylor ",
+      body: `Hello,\n\n
+
+      My name is ${name || "[YOUR NAME HERE]"}. I am a resident of ${state || "[STATE]"} and I am emailing today to demand accountability for the racist murder of Breonna Taylor.\n\n
+      
+      I demand that charges be pressed against all officers involved in this heinous killing, including Sgt. Jonathan Mattingly and officers Brett Hankison and Myles Cosgrove. They should all be fired, and should be charged and prosecuted to the fullest extent of the law for murder.\n\n
+      
+      Breonna Taylor was an ER technician, working tirelessly to help others during this global pandemic. She should be alive today. She would be alive today if it were not for the gross abuse of power and white supremacy exhibited by the Louisville Police Department. All officers involved must face consequences for this murder in order to provide her family with justice and to deter law enforcement from committing racist and brutal acts of violence against communities of color.\n\n
+      
+      In addition, I demand that we provide more support for community efforts and organizations that work to prevent police brutality and violence.\n\n
+      
+      Sincerely,\n
+      ${
+        name || "[YOUR NAME HERE]"
+      }`,
+      directRecipient: `meredythm@louisvilleco.gov`,
+      receivers = [
+        {
+          label: "Louisville Commonwealth Attorney",
+          name: "Tom Wine",
+          email: "tbwine@louisvilleprosecutor.com",
+        },
+        {
+          label: "Asst. Chief of Police",
+          name: "Robert Schroeder",
+          email: "robert.schroeder@louisvilleky.gov",
+        },
+      ],    
+      modalBody: `On March 13th, Breonna Taylor was killed by Louisville police as they illegally executed a drug raid at the wrong address for a person they had already arrested earlier that day. The LMPD officers responsible for Breonna’s murder are John Mattingly, Brett Hankison, and Myles Cosgrove. To date, they have been placed on administrative leave while the investigation continues. No charges have been brought and no arrests have been made.`,
+      modalTitle: `Email officials to bring Louisville police to justice `,
+      modalUrl: `https://www.standwithbre.com/`,
+    }
+  }
+  
+  export default buildEmail
+  
--- src/emails/police-brutality-la.js
+++ src/emails/police-brutality-la.js
@@ -6,6 +6,84 @@ function buildEmail({ name }) {
       name || "[YOUR NAME HERE]"
     }`,
     directRecipient: `clerk.budgetandfinancecommittee@lacity.org`,
+    receivers = [
+      {
+        label: "CD1",
+        name: "Gil Dedillo",
+        email: "councilmember.cedillo@la.org",
+      },
+      {
+        label: "CD2",
+        name: "Paul Krekorian",
+        email: "councilmember.Krekorian@lacity.org",
+      },
+      {
+        label: "CD3",
+        name: "Bob Blumenfield",
+        email: "councilmember.blumenfield@lacity.org",
+      },
+      {
+        label: "CD4",
+        name: "David Ryu",
+        email: "david.ryu@lacity.org",
+      },
+      {
+        label: "CD5",
+        name: "Paul Koretz",
+        email: "paul.koretz@lacity.org",
+      },
+      {
+        label: "CD6",
+        name: "Nuny Martinez",
+        email: "councilmember.martinez@lacity.org",
+      },
+      {
+        label: "CD7",
+        name: "Monica Rodriguez",
+        email: "councilmember.rodriguez@lacity.org",
+      },
+      {
+        label: "CD8",
+        name: "Marcqueece Harris-Dawson",
+        email: "councilmember.harris-dawson@lacity.org",
+      },
+      {
+        label: "CD9",
+        name: "Curren Price",
+        email: "councilmember.price@lacity.org",
+      },
+    
+      {
+        label: "CD10",
+        name: "Herb Wesson",
+        email: "councilmember.wesson@lacity.org",
+      },
+      {
+        label: "CD11",
+        name: "Mike Bonin ",
+        email: "councilmember.bonin@lacity.org",
+      },
+      {
+        label: "CD12",
+        name: "John Lee",
+        email: "councilmember.Lee@lacity.org",
+      },
+      {
+        label: "CD13",
+        name: "Mitch O’Farrell",
+        email: "councilmember.ofarrell@lacity.org",
+      },
+      {
+        label: "CD14",
+        name: "Jose Huizar (suspended)",
+        email: "councilmember.huizar@lacity.org",
+      },
+      {
+        label: "CD15",
+        name: "Joe Buscaino ",
+        email: "councilmember.buscaino@lacity.org",
+      },
+    ],    
     modalBody: `Mayor Garcetti has chosen to increase LAPD funding, at a time when Angelenos have made clear this does not align with their priorities and concerns. The City’s unemployment rate continues to rise — reaching around 55% in early May, and nationally the highest rate since the Great Depression. Many of its approximately 2 million renters are struggling to pay rent. 58,000 of our unhoused comrades remain on the streets, despite clear evidence that failing to house them is the equivalent of sentencing them to death. \n\nThe mayor has failed to commandeer hotel rooms, despite 100,000 rooms sitting vacant, many that were overwhelmingly built with public money or on public land. Incarcerated folx are not being released, though the risk of contracting COVID-19 is greater in congregate and institutionalized living settings. Garcetti has chosen to ignore these pressing concerns, furlough 16,000 non-police municipal workers, cut funding to services, and funnel more (and unnecessary) money towards the police department AS CRIME RATES HAVE DROPPED.`,
     modalTitle: `Email councilmembers to defund LAPD`,
     modalUrl: `https://docs.google.com/document/u/1/d/1qbq7YfJs102qJbGwGO1-wFa0diG16LdGYqiOoQ-hRAI/mobilebasic`,
