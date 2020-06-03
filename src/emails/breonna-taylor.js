6b8a46c9f9ec30278d7edf84ed95d71be1fab3e7
Michael Yu <michael.yu2016@gmail.com>
2020-06-03 16:47:33 -0700

    breona taylor signature format fix

--- src/emails/breonna-taylor.js
+++ src/emails/breonna-taylor.js
@@ -6,8 +6,9 @@ function buildEmail({ name, state }) {
       name || "[YOUR NAME HERE]"
     }. I am a resident of ${
       state || "[STATE]"
-    } and I am emailing today to demand accountability for the racist murder of Breonna Taylor.\n\nI demand that charges be pressed against all officers involved in this heinous killing, including Sgt. Jonathan Mattingly and officers Brett Hankison and Myles Cosgrove. They should all be fired, and should be charged and prosecuted to the fullest extent of the law for murder.\n\nBreonna Taylor was an ER technician, working tirelessly to help others during this global pandemic. She should be alive today. She would be alive today if it were not for the gross abuse of power and white supremacy exhibited by the Louisville Police Department. All officers involved must face consequences for this murder in order to provide her family with justice and to deter law enforcement from committing racist and brutal acts of violence against communities of color.\n\nIn addition, I demand that we provide more support for community efforts and organizations that work to prevent police brutality and violence.\n\n Sincerely,\n\n
-      ${name || "[YOUR NAME HERE]"}`,
+    } and I am emailing today to demand accountability for the racist murder of Breonna Taylor.\n\nI demand that charges be pressed against all officers involved in this heinous killing, including Sgt. Jonathan Mattingly and officers Brett Hankison and Myles Cosgrove. They should all be fired, and should be charged and prosecuted to the fullest extent of the law for murder.\n\nBreonna Taylor was an ER technician, working tirelessly to help others during this global pandemic. She should be alive today. She would be alive today if it were not for the gross abuse of power and white supremacy exhibited by the Louisville Police Department. All officers involved must face consequences for this murder in order to provide her family with justice and to deter law enforcement from committing racist and brutal acts of violence against communities of color.\n\nIn addition, I demand that we provide more support for community efforts and organizations that work to prevent police brutality and violence.\n\n Sincerely,\n\n${
+      name || "[YOUR NAME HERE]"
+    }`,
     args: {
       name: {
         label: "Your name",
