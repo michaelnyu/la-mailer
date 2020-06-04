07388d5eeabe4a0d07ca197fc4eb2ec2f28eace2
Bryan Wong <bryanw0ng@ucla.edu>
2020-06-03 21:15:02 -0700

    fix naming issue for preview component

--- src/components/ControlContainer.js
+++ src/components/ControlContainer.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Select from "react-select"
 import Receiver from "./receiver"
-import ControlAction from "./ControlAction"
 import { Spacer } from "./Spacer"
 import Button from "./Button"
 
