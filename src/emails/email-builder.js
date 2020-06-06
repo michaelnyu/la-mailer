6d3263cd0ba176b08d01486b62dad403fcea14f4
Bryan Wong <bryanw0ng@ucla.edu>
2020-06-05 23:41:44 -0700

    add defund opd

--- src/emails/email-builder.js
+++ src/emails/email-builder.js
@@ -1,17 +1,20 @@
 import PoliceBrutalityLa from "./police-brutality-la"
 import BreonnaTaylor from "./breonna-taylor"
 import JackieLacy from "./jackie-lacy"
+import DefundOpd from "./defund-opd"
 
 export const emailIdMap = {
   "police-brutality-la": PoliceBrutalityLa,
   "breonna-taylor": BreonnaTaylor,
   "jackie-lacy": JackieLacy,
+  "defund-opd": DefundOpd,
 }
 
 export const emailIdTitleMap = {
-  "police-brutality-la": "Defund LAPD Template",
-  "breonna-taylor": "Justice for Breonna Taylor Template",
-  "jackie-lacy": "LA - Rescind Jackie Lacey Endorsement Template",
+  "police-brutality-la": "Defund LAPD",
+  "breonna-taylor": "Justice for Breonna Taylor",
+  "jackie-lacy": "Rescind Jackie Lacey Endorsement",
+  "defund-opd": "Defund OPD",
 }
 
 export function buildEmailPreview({ emailId, stringInputs }) {
